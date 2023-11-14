1a9ca8540d3c82adb29b0f11cfc852de41b377ba
Tsosunam <t.osuna@uniandes.edu.co>
2023-11-13 23:07:39 -0500

    Se viene el proyecto del siglo

---
+++ Entrega-2/Project2_G11/src/Interfaz/MenuAdministradorGeneral.java
@@ -0,0 +1,11 @@
+package Interfaz;
+
+import java.awt.BorderLayout;
+import java.util.EventListener;
+import java.awt.Color;
+
+public class MenuAdministradorGeneral extends Jpanel implements ActionListener
+{
+    private JButton 
+    
+}
